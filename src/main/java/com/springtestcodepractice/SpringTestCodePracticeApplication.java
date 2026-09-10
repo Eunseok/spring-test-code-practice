3646176c85fc2a79f7d085d594f2a434dfc94808
Eunseok <asp6742@gmail.com>
2026-09-10 15:38:28 +0900

    initial commit

---
+++ src/main/java/com/springtestcodepractice/SpringTestCodePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springtestcodepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestCodePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestCodePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springtestcodepractice/SpringTestCodePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtestcodepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestCodePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
